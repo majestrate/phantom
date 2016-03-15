97d67586e0df96314706eae9bf7188114024e5b9
Jeff Becker <ampernand@gmail.com>
2016-03-15 14:48:51 -0400

    * makefile builds protobuf targets

--- protos/generate_protos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SRC=../src
-protoc-c --c_out=$SRC *proto
-echo -n -e 'PROTOOBJECTS=' > $SRC/protobufobjects
-for i in *proto; do echo -n -e "`echo $i | cut -d '.' -f 1`.pb-c.o ";done >> $SRC/protobufobjects
